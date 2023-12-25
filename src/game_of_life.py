19bf90157f662c28bbd0973e5369a43519a120a8
mike <mikesilaev@gmail.com>
2023-12-25 22:40:17 +0100

    modified singleton metaclass

--- src/game_of_life.py
+++ src/game_of_life.py
@@ -9,7 +9,8 @@ class SingletonMeta(type):
 
     def __call__(cls, *args, **kwargs):
         with cls._lock:
-            if cls not in cls._instances or args or kwargs:
+            #if cls not in cls._instances or args or kwargs:
+            if not cls._instances or args or kwargs:
                 instance = super().__call__(*args, **kwargs)
                 cls._instances[cls] = instance
                # print("privet")
